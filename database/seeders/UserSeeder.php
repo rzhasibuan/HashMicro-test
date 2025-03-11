12b6dcbb0ee777749aee6a32d84b002fd06bc561
rzhasibuanIsHere <rzhasibuan@gmail.com>
2025-03-11 13:36:43 +0700

    feat(web): add seeder user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@testing.com',
+            'password' => Hash::make('admin'),
+        ]);
+
+        // Dummy Users
+//        User::factory()->count(10)->create();
+
+    }
+}
