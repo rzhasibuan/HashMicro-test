0a02f114178c470d2939e133c8eaecc1feeec4d0
rzhasibuanIsHere <rzhasibuan@gmail.com>
2025-03-11 11:57:03 +0700

    feat(web): refactor character compare

--- app/Services/CharacterCompareService.php
+++ app/Services/CharacterCompareService.php
@@ -6,25 +6,36 @@ class CharacterCompareService extends Service
 {
     public function compare(string $character1, string $character2): array
     {
-        $chars = str_split($character1);
+        $character1 = strtoupper($character1);
+        $character2 = strtoupper($character2);
+
+        $chars1 = str_split($character1);
         $chars2 = str_split($character2);
 
         $matchCount = 0;
-        foreach ($chars as $index => $char) {
-            $key = array_search($char, $chars2);
-            if ($key !== false) {
-                $matchCount++;
-                unset($chars2[$key]);
+
+        for ($i = 0; $i < count($chars1); $i++) {
+
+            for ($j = 0; $j < count($chars2); $j++) {
+
+                if ($chars1[$i] === $chars2[$j]) {
+                    $matchCount++;
+                    unset($chars2[$j]);
+                    $chars2 = array_values($chars2);
+                    break;
+                }
+
             }
+
         }
 
-        $percentage = count($chars) > 0 ? ($matchCount / count($chars)) * 100 : 0;
-
+        $percentage = count($chars1) > 0 ? ($matchCount / count($chars1)) * 100 : 0;
         return $this->finalResultSuccess([
             "character1" => $character1,
             "character2" => $character2,
             "percentage" => $percentage,
         ]);
     }
+
 
 }
